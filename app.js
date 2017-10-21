4fb279df611aa6ff22301d4e70adcb6f3ae8d9f2
Arjun Mayur <arjunmayur@Arjuns-MacBook-Air.local>
2017-10-21 11:51:38 -0500

    re-routing works

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var tickets = require('./routes/tickets');
+var signUp = require('./routes/signUp');
 
 var app = express();
 
@@ -25,6 +26,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/tickets',tickets);
+app.use('/signUp',signUp);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
